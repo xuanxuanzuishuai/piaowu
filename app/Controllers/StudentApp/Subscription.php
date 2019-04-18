bac7f621ae1e1b7026d442d2cc90fae3ff05336e
yangyijie <yangyijie@xiaoyezi.com>
2019-04-18 20:50:34 +0800

    DSSCRM-0: [refactor] added Subscription
    Change-Id: I094b7312559bae87aa24b7e821a82ffc2d7d423c

---
+++ app/Controllers/StudentApp/Subscription.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: newtype0092
+ * Date: 2019/4/18
+ * Time: 8:44 PM
+ */
+
+namespace app\Controllers\StudentApp;
+
+use App\Controllers\ControllerBase;
+use App\Libs\MysqlDB;
+use App\Libs\Valid;
+use App\Services\StudentAppService;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Http\StatusCode;
+
+class Subscription extends ControllerBase
+{
+    public function redeemGiftCode(Request $request, Response $response)
+    {
+        $rules = [
+            [
+                'key' => 'code',
+                'type' => 'required',
+                'error_code' => 'active_code_is_required'
+            ]
+        ];
+
+        $params = $request->getParams();
+        $result = Valid::appValidate($params, $rules);
+
+        if ($result['code'] != Valid::CODE_SUCCESS) {
+            return $response->withJson($result, StatusCode::HTTP_OK);
+        }
+
+        $db = MysqlDB::getDB();
+        $db->beginTransaction();
+
+        list($errorCode, $ret) = StudentAppService::redeemGiftCode($params['code'], $this->ci['user']['id']);
+
+        if (!empty($errorCode)) {
+            $result = Valid::addAppErrors([], $errorCode);
+            return $response->withJson($result, StatusCode::HTTP_OK);
+        }
+
+        $db->commit();
+
+        return $response->withJson([
+            'code' => Valid::CODE_SUCCESS,
+            'data' => [
+                'result' => $ret
+            ]
+        ], StatusCode::HTTP_OK);
+    }
+}
