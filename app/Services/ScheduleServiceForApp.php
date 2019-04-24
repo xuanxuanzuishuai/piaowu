5fb5db9f9d3d267f83211716fe9d33d0a0d23b6b
dahua <liuhua@xiaoyezi.com>
2019-04-24 16:35:29 +0800

    DSSCRM-34: [feature] schedule end
    Change-Id: I0586d17516a855024004ab834e2918b53a3dddc1

--- app/Services/ScheduleServiceForApp.php
+++ app/Services/ScheduleServiceForApp.php
@@ -8,20 +8,74 @@
 
 namespace App\Services;
 
-use App\Libs\Constants;
-use App\Models\ScheduleModel;
+use App\Libs\SimpleLogger;
 use App\Models\ScheduleExtendModel;
-use App\Models\ScheduleTaskUserModel;
+use App\Models\ScheduleModelForApp;
 use App\Models\ScheduleUserModel;
 
 class ScheduleServiceForApp
 {
+    /**
+     * 1v1课程结束，我们在此时创建课程
+     * @param $schedule
+     * @return array
+     */
     public static function endSchedule($schedule){
-        // 处理课后单
-        $reportOk = ScheduleExtendModel::insertRecord($schedule);
-        if(!$reportOk){
-            return 'insert_report_fail';
+        // 插入schedule
+        $now = time();
+        $_schedule = [
+            'course_id' => 212, //TODO
+            'start_time' => $schedule['start_time'],
+            'end_time' => $now,
+            'duration' => $schedule['start_time'] - $now,
+            'status' => -1,
+            'org_id' => $schedule['org_id'],
+            'create_time' => $now,
+            'update_time' => $now,
+        ];
+        $scheduleId = ScheduleModelForApp::insertRecord($_schedule);
+        if(!$scheduleId){
+            return ['insert_schedule_fail', null];
         }
-        return null;
+
+        // 插入schedule_user
+        $schedule_user = [
+            'schedule_id' => $scheduleId,
+            'create_time' => $now,
+            'update_time' => $now,
+            'status' => 1,
+            'remark' => ''
+        ];
+        $schedule_student = $schedule_user;
+        $schedule_student['user_id'] = $schedule['student_id'];
+        $schedule_student['user_role'] = 1;
+        $schedule_student['user_status'] = ScheduleUserModel::STUDENT_STATUS_ATTEND;
+        $schedule_teacher = $schedule_user;
+        $schedule_teacher['user_id'] = $schedule['teacher_id'];
+        $schedule_teacher['user_role'] = 2;
+        $schedule_teacher['user_status'] = ScheduleUserModel::TEACHER_STATUS_ATTEND;
+        $scheduleUsers = [$schedule_student, $schedule_teacher];
+        $scheduleUserOk = ScheduleUserModel::batchInsert($scheduleUsers, false);
+        if(!$scheduleUserOk){
+            return ['insert_schedule_user_fail', null];
+        }
+
+        // 处理课后单
+        $report = [];
+        $report['schedule_id'] = $scheduleId;
+        $report['course_id'] = 212;
+        $report['opn_lessons'] = implode(",", $schedule['lessons']);
+        $report['remark'] = $schedule['report']['remark'];
+        $report['class_score'] = $schedule['report']['class_score'];
+        $report['detail_score'] = json_encode([
+            'homework_rank' => $schedule['report']['homework_rank'],
+            'performance_rank' => $schedule['report']['performance_rank']
+        ]);
+        $reportOk = ScheduleExtendModel::insertReport($report);
+        if(!$reportOk){
+            return ['insert_report_fail', null];
+        }
+
+        return [null, $scheduleId];
     }
 }
