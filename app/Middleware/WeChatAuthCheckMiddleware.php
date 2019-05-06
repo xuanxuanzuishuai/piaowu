ac7f28dfb3989c67345b2387fa8f25b77188ab73
tonghuameng <tonghuameng@xiaoyezi.com>
2019-05-06 12:01:35 +0800

    DSSCRM-0: [feature] check the user_type of the token
    Change-Id: Id17a5bec171417dc3f8c3a4e18a4bba0e7f46f6e

--- app/Middleware/WeChatAuthCheckMiddleware.php
+++ app/Middleware/WeChatAuthCheckMiddleware.php
@@ -9,13 +9,15 @@
 namespace App\Middleware;
 
 use App\Libs\SimpleLogger;
+use App\Libs\UserCenter;
 use Slim\Http\Request;
 use App\Libs\Valid;
 use Slim\Http\StatusCode;
 use Slim\Http\Response;
 use App\Services\WeChatService;
 
-/** 这个必须要和WeChatOpenIdCheckMiddleware连用，
+/**
+ * 根据token获取用户信息，如果获取不到用户信息，则返回token_expired报错，客户端需要捕获该报错然后调用login接口
  * Class WeChatAuthCheckMiddleware
  * @package App\Middleware
  */
@@ -39,6 +41,17 @@ class WeChatAuthCheckMiddleware extends MiddlewareBase
             return $response->withJson(Valid::addAppErrors([], 'token_expired'), StatusCode::HTTP_OK);
         }
 
+        $needle = "/student_wx";
+        $length = strlen($needle);
+        $user_type = 2;
+        $currentUrl = $request->getUri()->getPath();
+        if (substr($currentUrl, 0, $length) === $needle){
+            $user_type = 1;
+        }
+        // 根据url确认哪种类型的用户，并检查当前token中保存的信息是否是该用户
+        if ($user_type != (int)$userInfo["user_type"]){
+            return $response->withJson(Valid::addAppErrors([], 'token_expired'), StatusCode::HTTP_OK);
+        }
         WeChatService::refreshToken($token);
         $this->container['user_info'] = $userInfo;
         $this->container["open_id"] = $userInfo["open_id"];
