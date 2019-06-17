e484f81026415134d8ae8496b346e9fb27eee838
yangyijie <yangyijie@xiaoyezi.com>
2019-06-17 16:25:21 +0800

    DSSCRM-0: [feature] 20190618 promotion
    Change-Id: Ib39c645a8aeb6ac08d8caeda45cb2dea5546c0e9

--- app/Controllers/OrgWeb/Erp.php
+++ app/Controllers/OrgWeb/Erp.php
@@ -65,6 +65,13 @@ class Erp extends ControllerBase
         $giftCodeNum = 1;
         $giftCodeUnit = GiftCodeModel::CODE_TIME_YEAR;
 
+        // 6月18日活动：6月30日前换购时间增加至1.5年(18个月)
+        $date = date('Ymd');
+        if ($date >= '20190618' && $date <= '20190630') {
+            $giftCodeNum = 18;
+            $giftCodeUnit = GiftCodeModel::CODE_TIME_MONTH;
+        }
+
         $ret = ErpService::exchangeGiftCode(
             [
                 'uuid' => $params['uuid'],
