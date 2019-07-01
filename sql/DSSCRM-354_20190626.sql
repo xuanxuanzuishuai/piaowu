41b9626ea28f88edc21676109f2a2d6ed64a4f46
yangyijie <yangyijie@xiaoyezi.com>
2019-07-01 11:10:50 +0800

    DSSCRM-354: [feature] sql default value
    Change-Id: I530c30f0f15543f0c2647936d10b231ca5588eb6

--- sql/DSSCRM-354_20190626.sql
+++ sql/DSSCRM-354_20190626.sql
@@ -1,3 +1,9 @@
 ALTER TABLE `student`
   ADD COLUMN `trial_start_date` INT(11) NULL DEFAULT NULL COMMENT '试用开始时间' AFTER `sub_end_date`,
   ADD COLUMN `trial_end_date` INT(11) NULL DEFAULT NULL COMMENT '试用结束时间' AFTER `trial_start_date`;
+
+ALTER TABLE `student`
+  CHANGE COLUMN `sub_start_date` `sub_start_date` INT(11) NOT NULL DEFAULT 0 COMMENT '订阅开始日期' ,
+  CHANGE COLUMN `sub_end_date` `sub_end_date` INT(11) NOT NULL DEFAULT 0 COMMENT '订阅结束日期' ,
+  CHANGE COLUMN `trial_start_date` `trial_start_date` INT(11) NOT NULL DEFAULT 0 COMMENT '试用开始时间' ,
+  CHANGE COLUMN `trial_end_date` `trial_end_date` INT(11) NOT NULL DEFAULT 0 COMMENT '试用结束时间' ;
