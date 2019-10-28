00213bb75d5e83bfaaaa0343a483c8778997679a
yangyijie <yangyijie@xiaoyezi.com>
2019-10-28 09:53:37 +0800

    DSSCRM-0: [feature] common request method
    Change-Id: I4b6e7496d3571b1d31469874ce38d1ad7a1f06fb

--- app/Libs/HttpHelper.php
+++ app/Libs/HttpHelper.php
@@ -8,6 +8,7 @@
 
 namespace App\Libs;
 
+use GuzzleHttp\Client;
 use Slim\Http\Response;
 use Slim\Http\StatusCode;
 
@@ -34,5 +35,47 @@ class HttpHelper
             'errors' => $errors,
         ];
         return $response->withJson($result, StatusCode::HTTP_OK);
+    }
+
+    /**
+     * @param $api
+     * @param array $params
+     * @param string $method
+     * @return bool|array
+     */
+    public static function requestJson($api,  $params = [], $method = 'GET')
+    {
+        try {
+            $client = new Client(['debug' => false]);
+
+            if ($method == 'GET') {
+                $data = ['query' => $params];
+            } elseif ($method == 'POST') {
+                $data = ['json' => $params];
+                $data['headers'] = ['Content-Type' => 'application/json'];
+            } else {
+                return false;
+            }
+
+            SimpleLogger::info("[HttpHelper] send request", ['api' => $api, 'data' => $data]);
+            $response = $client->request($method, $api, $data);
+            $body = $response->getBody()->getContents();
+            $status = $response->getStatusCode();
+            SimpleLogger::info("[HttpHelper] send request ok", ['body' => $body, 'status' => $status]);
+
+            if (($status != StatusCode::HTTP_OK)) {
+                return false;
+            }
+
+            $res = json_decode($body, true);
+
+        } catch (\Exception $e) {
+            SimpleLogger::error("[HttpHelper] send request error", [
+                'error_message' => $e->getMessage()
+            ]);
+            return false;
+        }
+
+        return $res;
     }
 }
