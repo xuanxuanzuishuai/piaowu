0773bf9b2afae9cec20913c72a3d8c02ed872346
yangyijie <yangyijie@xiaoyezi.com>
2019-10-30 11:39:17 +0800

    AIPL-3096: [feature] added oss update type app_log
    Change-Id: Ib5b43b58425cffad2f9a8605460d1a01c2b0fa49

--- app/Libs/AliOSS.php
+++ app/Libs/AliOSS.php
@@ -21,6 +21,7 @@ class AliOSS
     const DIR_DYNAMIC_MIDI = 'dynamic_midi';
     const DIR_AUDIO_COMMENT = 'audio_comment'; // 老师课堂点评语音
     const DIR_HOMEWORK_AUDIO_COMMENT = 'homework_audio_comment'; // 课后作业点评语音
+    const DIR_APP_LOG = 'app_log'; // app日志
 
     const PROCESS_STYLE_NOTE_THUMB = 'note_thumb'; // 老师笔记缩略图 image/auto-orient,1/resize,p_25/quality,q_70
 
@@ -341,6 +342,7 @@ class AliOSS
             self::DIR_DYNAMIC_MIDI,
             self::DIR_AUDIO_COMMENT,
             self::DIR_HOMEWORK_AUDIO_COMMENT,
+            self::DIR_APP_LOG,
         ];
         if (!in_array($dirType, $typeConstants)) {
             return null;
