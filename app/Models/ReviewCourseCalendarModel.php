5bb54799a7d9964b966b60afd9226d832d9724e4
yangyijie <yangyijie@xiaoyezi.com>
2020-01-07 20:47:55 +0800

    DSSCRM-504: [feature] review course calendar
    Change-Id: I65e565b6330f4a5296c8f865a7753277d3e8110c

---
+++ app/Models/ReviewCourseCalendarModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: newtype0092
+ * Date: 2020/1/7
+ * Time: 8:43 PM
+ */
+
+namespace App\Models;
+
+
+class ReviewCourseCalendarModel extends Model
+{
+    public static $table = 'review_course_calendar';
+}
--- app/Services/ReviewCourseCalendarService.php
+++ app/Services/ReviewCourseCalendarService.php
@@ -9,6 +9,10 @@
 namespace App\Services;
 
 
+use App\Libs\Constants;
+use App\Libs\SimpleLogger;
+use App\Models\ReviewCourseCalendarModel;
+
 class ReviewCourseCalendarService
 {
     /**
@@ -18,9 +22,47 @@ class ReviewCourseCalendarService
      */
     public static function getReviewTimeWindow($reviewDate)
     {
-        $startTime = strtotime("$reviewDate -1 day");
-        $endTime = $startTime + 86399;
+        // 结束时间为点评日期当天0点
+        $endTime = strtotime($reviewDate);
+        if (empty($endTime)) {
+            SimpleLogger::error('getReviewTimeWindow: error review date', ['$reviewDate' => $reviewDate]);
+            return [null, null];
+        }
 
-        return [$startTime, $endTime];
+        $dateConfig = ReviewCourseCalendarModel::getRecord(
+            ['review_date' => $reviewDate, 'status' => Constants::STATUS_TRUE], '*', false);
+        $dayOfWeek = date("w");
+
+        SimpleLogger::debug('getReviewTimeWindow: date config', [
+            '$dateConfig' => $dateConfig,
+            '$dayOfWeek' => $dayOfWeek
+        ]);
+
+        if (!empty($dateConfig)) {
+
+            // 开始时间默认为设置的点评日期前一天0点
+            $startTime = strtotime($dateConfig['play_date']);
+
+            // play_date 设置为0时表示对应的 review_date 为休息日不点评
+            if (empty($startTime)) {
+                SimpleLogger::debug('getReviewTimeWindow: return null, play_date = 0', []);
+                return [null, null];
+            }
+
+        } elseif ($dayOfWeek == 6 || $dayOfWeek == 0) {
+            // 周末休息不点评
+            SimpleLogger::debug('getReviewTimeWindow: return null, weekend', []);
+            return [null, null];
+
+        } elseif ($dayOfWeek == 1) {
+            // 周一点评开始时间为周五(前三前)0点
+            $startTime = $endTime - (86400*3);
+
+        } else {
+            // 开始时间默认为点评日期前一天0点
+            $startTime = $endTime - 86400;
+        }
+
+        return [$startTime, $endTime-1];
     }
 }
