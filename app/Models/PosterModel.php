bdc8a2f397bbb180795bb2fb55cea845d0538d9a
lianglipeng <lianglipeng@xiaoyezi.com>
2020-03-16 15:44:05 +0800

    DSSCRM-573: [fix] dss crm wechat poster config setting
    Change-Id: I8a59204e43ecdba6c4115f7fc7b6444134c9f75e

--- app/Models/PosterModel.php
+++ app/Models/PosterModel.php
@@ -23,24 +23,24 @@ class PosterModel extends Model
     //此版本先把配置文件以配置形式写入，等版本更新在做成后台可操作配置方式
     public static $settingConfig = [
         self::APPLY_TYPE_STUDENT_WECHAT => [
-            'qr_x' => 60,
-            'qr_y' => 60,
+            'qr_x' => 533,
+            'qr_y' => 92,
             'poster_width' => 750,
-            'poster_height' => 1050,
-            'qr_width' => 100,
-            'qr_height' => 100,
+            'poster_height' => 1334,
+            'qr_width' => 154,
+            'qr_height' => 154,
             'content1' => '跟我做，只需两步！领取现金红包！
 【1】复制以下文案+海报发送至朋友圈或群聊
 【2】好友付费训练营体验即可获得现金红包奖励，多邀多得上不封顶',
             'content2' => '没有明确目标，孩子不爱练琴?无法实时纠错，琴童摸不着头脑?快来试试练琴神器小叶子智能陪练!拒绝枯燥无趣，让问题不过夜，让孩子成就满满!',
         ],
         self::APPLY_TYPE_TEACHER_WECHAT => [
-            'qr_x' => 60,
-            'qr_y' => 60,
+            'qr_x' => 533,
+            'qr_y' => 92,
             'poster_width' => 750,
-            'poster_height' => 1050,
-            'qr_width' => 100,
-            'qr_height' => 100,
+            'poster_height' => 1334,
+            'qr_width' => 154,
+            'qr_height' => 154,
             'content1' => '',
             'content2' => '',
         ]
