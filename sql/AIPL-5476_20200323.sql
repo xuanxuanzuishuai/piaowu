cdefb727c6c147bf7f5e867f13e18c9af10c5adf
zhaijinghui <zhaijinghui@xiaoyezi.com>
2020-03-23 17:40:51 +0800

    AIPL-5476: [feature] add text of subscribe; modify poster content
    Change-Id: I8ce6b76133db79cd8d5ac734fb3f7f11a030b3a6

---
+++ sql/AIPL-5476_20200323.sql
@@ -0,0 +1,19 @@
+SET NAMES utf8mb4; -- 以下sql有emoji表情，执行前需先设置会话使用的字符编码
+INSERT INTO `dss_pre`.`wechat_config` (`type`, `content`, `msg_type`, `content_type`, `event_type`, `event_key`, `create_time`, `update_time`, `create_uid`, `update_uid`) VALUES (1, '嗨！小叶子欢迎您[爱心]
+陪孩子练琴，您辛苦了[玫瑰]
+
+小叶子智能陪练，是清华爸爸团队联合国内外钢琴教育专家打造的全球首创，AI驱动的陪练App。
+
+🎹练琴想要错音少、效率高，
+🍭科学、高效的方法很重要。
+
+🎉小叶子智能陪练App🎉
+AI实时纠正错音，孩子练琴不盲目
+练完打分有排行，孩子练琴兴趣高
+每天发练琴报告，妈妈再也不用陪
+
+点击观看👇
+<a href="http://mp.weixin.qq.com/s?__biz=MzU1NTkxNzY3MQ==&mid=100000231&idx=1&sn=d11c0cfa5a19418dab0c99c1d8b9b77a&chksm=7bcc49ef4cbbc0f9f923570f89f7389933ee84750cd1751f8be23084d4311f740aff1d9c8775#rd">钢琴大师郎朗演奏《肖邦练习曲》，亲自测试小叶子智能陪练，赞不绝口！</a>
+
+点击体验👇
+<a href="https://referral.xiaoyezi.com/market/landing?channel_id=1244">限时49元2周无限次使用！</a>', 'event', 1, 'subscribe', 'subscribe', 1583918495, 0, 0, 0);
