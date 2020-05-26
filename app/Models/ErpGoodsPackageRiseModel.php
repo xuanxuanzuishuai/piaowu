a13dc6d0a103fe0223d369da02181794d2752a79
yangyijie <yangyijie@xiaoyezi.com>
2020-05-26 18:52:23 +0800

    DSSCRM-661: [feature] readonly model
    Change-Id: I12da4004fbcc2ea6d7a9243385d4d5b51bea3242

---
+++ app/Models/ErpGoodsPackageRiseModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: newtype0092
+ * Date: 2020/5/26
+ * Time: 6:19 PM
+ */
+
+namespace App\Models;
+
+
+class ErpGoodsPackageRiseModel extends ReadonlyModel
+{
+    protected static $table = 'erp_goods_package_rise';
+}
