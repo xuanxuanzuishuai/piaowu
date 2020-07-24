63c052922614ee442322b0f17782dedcf2f8195e
yuxuan <yuxuan@xiaoyezi.com>
2020-07-24 11:10:18 +0000

    DSSCRM-787: [feature] change word 2
    Change-Id: Ib8b383682c070a8ee4d8cbf29f895d3363224413

--- app/Services/CollectionService.php
+++ app/Services/CollectionService.php
@@ -262,7 +262,7 @@ class CollectionService
             $where .= " and a.trial_type=" . (int)$params['trial_type'];
         }
 
-        if ($params['task_id']) {
+        if ($params['task_id'] !== "") {
             $where .= " and a.task_id=" . $params['task_id'];
         }
 
