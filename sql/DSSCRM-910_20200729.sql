92b20f3c722d950690a1cea0472f208431147cc2
liqian <liqian@xiaoyezi.com>
2020-08-05 17:48:03 +0800

    DSSCRM-910: [feature] Change the domain name to obtain OCR
    Change-Id: Ib32151bce48ecb3c7ba53eb306f682623b5e85ff

--- sql/DSSCRM-910_20200729.sql
+++ sql/DSSCRM-910_20200729.sql
@@ -1,1 +1,1 @@
-INSERT INTO `dict` (`type`, `key_name`, `key_code`, `key_value`, `desc`) VALUES ('APP_CONFIG_STUDENT', 'AI练琴后端设置', 'request_ocr_search_service', 'readscore.xiaoyezi.com', '请求OCR的搜索服务');
+INSERT INTO `dict` (`type`, `key_name`, `key_code`, `key_value`, `desc`) VALUES ('APP_CONFIG_STUDENT', 'AI练琴后端设置', 'request_ocr_search_service', 'https://readscore.xiaoyezi.com', '请求OCR的搜索服务');
