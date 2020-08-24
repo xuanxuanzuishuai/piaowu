50f7bbf8d2bcce3ff2b622cdbacda78c48a6c851
yangyijie <yangyijie@xiaoyezi.com>
2020-08-24 14:57:54 +0800

    DSSCRM-984: [feature] 3.update sql, serve_app_id not null
    Change-Id: I45972a5668df690b273c0c43d9e07d70cbeaee43

--- sql/DSSCRM-984_20200820.sql
+++ sql/DSSCRM-984_20200820.sql
@@ -3,5 +3,5 @@ INSERT INTO `dict` (`type`, `key_name`, `key_code`, `key_value`) VALUES ('PACKAG
 
 # has_used 是之前的废弃字段 换成新字段 上线所有学生默认为8
 ALTER TABLE `student`
-CHANGE COLUMN `has_used` `serve_app_id` TINYINT NULL DEFAULT 8 COMMENT '售后服务归属方 1 真人 8 智能' ;
+CHANGE COLUMN `has_used` `serve_app_id` TINYINT NOT NULL DEFAULT 8 COMMENT '售后服务归属方 1 真人 8 智能' ;
 UPDATE `student` SET `serve_app_id` = 8;
