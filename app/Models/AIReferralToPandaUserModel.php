bb89bb4b46350fbc59c01249467ce65139803316
zhaofengqiang <zhaofengqiang@xiaoyezi.com>
2020-09-23 14:26:20 +0800

    TOCRM-1114: [feature] send ai_referral_to_panda_user template message
    Change-Id: I482299de60cc165adc50d745de173171e5f8099c

--- app/Models/AIReferralToPandaUserModel.php
+++ app/Models/AIReferralToPandaUserModel.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 
+use App\Libs\MysqlDB;
+
+
 class AIReferralToPandaUserModel extends Model
 {
     static $table = 'ai_referral_to_panda_user';
@@ -18,4 +21,48 @@ class AIReferralToPandaUserModel extends Model
 
     const USER_SEND_SUCCESS = 1; // 发送成功
     const USER_SEND_FAILED = 2;  // 发送失败
+
+
+    /**
+     * 查询待发送模板消息用户信息
+     * @param $userType
+     * @param $date
+     * @return array
+     */
+    public static function getToSentStudentInfo($userType, $date)
+    {
+        if (is_array($userType)) {
+            $userType = implode(', ', $userType);
+        }
+        $db = MysqlDB::getDB();
+        $sql = "SELECT 
+       u.id,
+       u.student_id,
+       uw.open_id,
+       s.mobile
+    FROM 
+         " . self::$table . " as u 
+        INNER JOIN "
+            . UserWeixinModel::$table . " as uw ON u.student_id = uw.user_id 
+            AND uw.app_id = 8
+            AND uw.user_type = 1
+            AND uw.busi_type = 1
+            AND uw.status = 1
+        INNER JOIN "
+            . StudentModel::$table . " as s ON s.id = uw.user_id
+    WHERE 
+        u.is_send=0
+        AND u.is_subscribe = 0
+        AND u.user_type in ( :user_type )
+        AND u.create_time >= :start_time
+        AND u.create_time <= :end_time;";
+
+        $map = [
+            ':user_type' => $userType,
+            ':start_time' => strtotime($date),
+            ':end_time' => strtotime($date . " 23:59:59"),
+        ];
+        $studentInfo = $db->queryAll($sql, $map);
+        return $studentInfo ?? [];
+    }
 }
--- app/Services/AIReferralToPandaUserService.php
+++ app/Services/AIReferralToPandaUserService.php
@@ -3,9 +3,12 @@
 namespace App\Services;
 
 
+use App\Libs\Util;
 use App\Libs\Valid;
 use App\Models\AIReferralToPandaUserModel;
-
+use App\Services\Queue\PushMessageTopic;
+use App\Libs\SimpleLogger;
+use Exception;
 
 class AIReferralToPandaUserService
 {
@@ -60,5 +63,81 @@ class AIReferralToPandaUserService
             return [];
         }
         return AIReferralToPandaUserModel::getRecords(['student_id' => $studentIds], false);
+    }
+
+    /**
+     * 发送真人报名模板消息
+     * @param $date
+     * @return array|null
+     */
+    public static function sendAiReferralToPandaNotice($date)
+    {
+        $userType = [AIReferralToPandaUserModel::USER_TYPE_LT4D, AIReferralToPandaUserModel::USER_TYPE_LT8D];
+        $studentInfo = AIReferralToPandaUserModel::getToSentStudentInfo($userType, $date);
+        if (empty($studentInfo)) {
+            return [0, 0];
+        }
+        $url = $_ENV['PANDA_WECHAT_FRONT_DOMAIN'] .'/#/webSignup?mobile=';
+        $templateId = $_ENV["AI_REFERRAL_TO_PANDA_NOTICE_TEMPLATE"];
+        $data = [
+            'first' => [
+                'value' => "家长您好，根据宝贝的练琴数据，已成功为您预约小叶子真人1对1钢琴陪练试听课一节，价值299元，限时免费体验！",
+            ],
+            'keyword1' => [
+                'value' => "智能陪练，让宝贝弹准弹对，扎实练琴硬实力；\n真人陪练，帮宝贝精炼细节，提升练琴软实力。",
+            ],
+            'keyword2' => [
+                'value' => "智能+真人结合使用，全面帮助孩子纠正练琴时出现的问题，效率翻倍。这可能是最适合宝贝的陪练课程，千万别错过！",
+            ],
+            'keyword3' => [
+                'value' => "点击查看详情，即可立即领取噢～",
+            ],
+        ];
+        $msgBody = [
+            'wx_push_type' => 'template',
+            'template_id' => $templateId,
+            'data' => $data,
+            'open_id' => '',
+        ];
+
+        try {
+            $topic = new PushMessageTopic();
+        } catch (Exception $e) {
+            Util::errorCapture('PushMessageTopic init failure', ['$dateTime' => time(),]);
+            return [0, 0];
+        }
+
+        $successIds = [];
+        $failedIds = [];
+        foreach ($studentInfo as $info) {
+            $msgBody['open_id'] = $info['open_id'];
+            $msgBody['url'] = $url . $info['mobile'];
+            try {
+                $topic->wxPushCommon($msgBody)->publish(rand(0, 600));
+                $successIds[] = $info['id'];
+            } catch (Exception $e) {
+                SimpleLogger::error("send notice send failure", ['info' => $info]);
+                $failedIds[] = $info['id'];
+                continue;
+            }
+        }
+        return [$successIds, $failedIds];
+    }
+
+    /**
+     * 更新发送状态
+     * @param $sendStatus
+     * @param $ids
+     * @return int|null
+     */
+    public static function modifyStatus($ids, $sendStatus)
+    {
+        $data = [
+            'is_send' => AIReferralToPandaUserModel::USER_IS_SEND,
+            'is_subscribe' => AIReferralToPandaUserModel::USER_IS_SUBSCRIBE,
+            'send_status' => $sendStatus,
+            'update_time' => time()
+        ];
+        return AIReferralToPandaUserModel::updateRecord($ids, $data, false);
     }
 }
