27a5a21162d04529c846c045245fd2c6fcc4b85e
liqian <liqian@xiaoyezi.com>
2020-10-15 16:47:51 +0800

    AIPL-9264: [fix] update sql
    Change-Id: I55fc96928c264c3358cf096393ebd8f4c3607e2e

--- sql/AIPL-9264_20200927.sql
+++ sql/AIPL-9264_20200927.sql
@@ -1,3 +1,3 @@
-select @event_task_id:=id from erp_event_task where name = "每日上完1节课";
+select @event_task_id:=id from erp_event_task where name = "本日完成一节课";
 INSERT INTO `dict` (`type`, `key_name`, `key_code`, `key_value`)
 VALUES ('credit_activity_config', '积分活动', 'attend_class_task_id', @event_task_id),
