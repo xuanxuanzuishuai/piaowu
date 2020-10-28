2925e7872c23dfeef81a4e165b5e11238f893611
liuguokun <liuguokun@xiaoyezi.com>
2020-10-28 15:16:32 +0800

    DSSCRM-1220: [feature] adjust huawei params name
    Change-Id: Ia24932607fdcc14909754cb0b5c51c7126d2e5e3

--- app/Controllers/StudentApp/App.php
+++ app/Controllers/StudentApp/App.php
@@ -129,12 +129,7 @@ class App extends ControllerBase
 
         $trackParams['platform'] = $platformId;
         $trackParams['ad_channel'] = $params['ad_channel'] ?? 0;
-
-        switch ($params['ad_channel']){
-            case TrackService::CHANNEL_HUAWEI:
-                $trackParams['ad_id'] = $params['track_id'] ?? 0;
-                break;
-        }
+        $trackParams['ad_id'] = $params['ad_id'] ?? '';
 
         $trackData = TrackService::trackEvent(TrackService::TRACK_EVENT_ACTIVE, $trackParams);
         $result = [
