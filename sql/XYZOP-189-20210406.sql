fccfc958133e5f594922062c52312576f670bc13
lizao <lizao@xiaoyezi.com>
2021-04-07 11:46:41 +0800

    XYZOP-189: [fix] agent callbacks

--- sql/XYZOP-189-20210406.sql
+++ sql/XYZOP-189-20210406.sql
@@ -1,1 +1,7 @@
 UPDATE `dict` SET `key_value` = '{"1":3095, "2":3096, "3":3097, "4":3096}' WHERE `key_code` = 'channel_dict' AND `type` = 'AGENT_CONFIG';
+
+INSERT INTO `dict` (`type`, `key_name`, `key_code`, `key_value`, `desc`)
+VALUES
+ ('AGENT_WEB_STUDENT_CONFIG', '代理支付回调', 'success_url_v1', 'https://referral.xiaoyezi.com/operation/pay?', '代理非微信调支付宝回调'),
+  ('AGENT_WEB_STUDENT_CONFIG', '代理支付回调', 'cancel_url_v1', 'https://referral.xiaoyezi.com/operation/pay?', '代理非微信调支付宝回调'),
+  ('AGENT_WEB_STUDENT_CONFIG', '代理支付回调', 'result_url_v1', 'https://referral.xiaoyezi.com/operation/pay?', '代理非微信调支付宝回调');
