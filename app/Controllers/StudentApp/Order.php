7fd6c352c671d0283647f89841023f59d9a23ad8
guoliangchang <18239051727@163.com>
2021-09-17 20:09:39 +0800

    XYZOP-1093 : [feature] work4

--- app/Controllers/StudentApp/Order.php
+++ app/Controllers/StudentApp/Order.php
@@ -8,8 +8,16 @@ use App\Controllers\ControllerBase;
 use App\Libs\Dss;
 use App\Libs\Exceptions\RunTimeException;
 use App\Libs\HttpHelper;
+use App\Libs\RC4;
+use App\Libs\Util;
 use App\Libs\Valid;
-use App\Services\OrderService;
+use App\Models\Dss\DssErpPackageV1Model;
+use App\Models\Dss\DssStudentModel;
+use App\Models\Dss\DssUserWeiXinModel;
+use App\Models\Erp\ErpPackageV1Model;
+use App\Services\BillMapService;
+use App\Services\ErpOrderV1Service;
+use App\Services\PayServices;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Slim\Http\StatusCode;
@@ -47,13 +55,40 @@ class Order extends ControllerBase
             return $response->withJson($result, StatusCode::HTTP_OK);
         }
         try {
-            $params = $request->getParams();
-            $params['student_id'] = $this->ci['user_info']['user_id'];
-            $data = OrderService::createAppBill($params);
-        } catch (RunTimeException $e) {
-            return HttpHelper::buildErrorResponse($response, $e->getWebErrorData());
+            $studentId = $this->ci['user_info']['user_id'] ?? '';
+            $studentInfo = DssStudentModel::getById($studentId);
+            if (empty($studentInfo)) {
+                throw new RunTimeException(['student_not_found']);
+            }
+
+            $packageInfo = DssErpPackageV1Model::getPackageById($params['package_id']);
+            if (empty($packageInfo) || $packageInfo['package_status'] != DssErpPackageV1Model::STATUS_ON_SALE) {
+                throw new RunTimeException(['package_not_available']);
+            }
+            $sceneData = [];
+            if (!empty($params['channel_id'])) {
+                $sceneData['c'] = $params['channel_id'];
+            }
+            $studentInfo['address_id'] = $params['address_id'] ?? true;
+            $studentInfo['package_sub_type'] = $packageInfo['sub_type'];
+            $employeeUuid = !empty($params['employee_id']) ? RC4::decrypt($_ENV['COOKIE_SECURITY_KEY'], $params['employee_id']) : null;
+            $channel = Util::isWx() ? ErpPackageV1Model::CHANNEL_WX : ErpPackageV1Model::CHANNEL_H5;
+            $payChannel = PayServices::payChannelToV1($params['pay_channel']);
+
+
+            $userWeixin = DssUserWeiXinModel::getByUserId($studentId);
+            $studentInfo['open_id'] = $userWeixin['open_id'] ?? null;
+            $giftGoods = $params['gift_res'] ?? [];
+            $payType = $params['pay_type'] ?? 1;
+            $ret = ErpOrderV1Service::createOrder($params['package_id'], $studentInfo, $payChannel, $payType, $employeeUuid, $channel, $giftGoods);
+            if (!empty($sceneData) && !empty($ret['order_id'])) {
+                // 保存agent_bill_map数据
+                BillMapService::mapDataRecord($sceneData, $ret['order_id'], $studentInfo['id']);
+            }
+        } catch (RuntimeException $e) {
+            return HttpHelper::buildErrorResponse($response, $e->getAppErrorData());
         }
-        return HttpHelper::buildResponse($response, $data);
+        return HttpHelper::buildResponse($response, $ret);
     }
 
     /**
