c5dc503f2ed10858a0dc5f85c9304f01a22cad95
yuxuan <yuxuan@xiaoyezi.com>
2022-03-22 12:47:30 +0800

    XYZOP-1588: [feature] add bill map open id

--- sql/XYZOP-1588-20220322.sql
+++ sql/XYZOP-1588-20220322.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `bill_map`
 ADD COLUMN `open_id` varchar(32) NOT NULL DEFAULT '' COMMENT '下单人的open_id,不是所有单子都有' AFTER `type`,
+ADD COLUMN `is_success` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否支付成功' AFTER `open_id`,
 ADD INDEX `idx_open_id`(`open_id`) USING BTREE;
