69f497bce9d1642a479289e2bdfc832218b6fb56
lianqingfeng <lianqingfeng@xiaoyezi.com>
2022-05-13 11:05:38 +0800

    XYZOP-1707: [feature] rw activity check is student add assistant v2

--- app/Services/StudentServices/DssStudentService.php
+++ app/Services/StudentServices/DssStudentService.php
@@ -3,6 +3,7 @@
 namespace App\Services\StudentServices;
 
 
+use App\Libs\Dss;
 use App\Libs\Exceptions\RunTimeException;
 use App\Models\Dss\DssStudentModel;
 use App\Services\Employee\DssEmployeeService;
@@ -20,7 +21,7 @@ class DssStudentService
     {
         $returnData = [
             'is_add_assistant_wx' => 0,
-            'assistant_info' => [],
+            'assistant_info'      => [],
         ];
         $studentInfo = DssStudentModel::getRecord(['id' => $studentId], ['assistant_id', 'is_add_assistant_wx']);
         if (empty($studentInfo)) {
@@ -33,7 +34,11 @@ class DssStudentService
         }
         // 获取助教信息
         $assistantInfo = DssEmployeeService::getEmployeeInfoById($studentInfo['assistant_id'], $fields);
-        is_array($assistantInfo) && $returnData['assistant_info'] = $assistantInfo;
+        if (!empty($assistantInfo)) {
+            // 如果助教信息存在：获取助教配置（app中需要的跳转信息）
+            $appAssistantInfo = (new Dss())->getWxAppAssistant(['assistant_id' => $studentInfo['assistant_id']]);
+            $returnData['assistant_info'] = array_merge($assistantInfo, $appAssistantInfo ?? []);
+        }
         return $returnData;
     }
 }
