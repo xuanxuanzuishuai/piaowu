e1263c322e35fa214f01ae49fa8d485f784df41a
yuxuan <yuxuan@xiaoyezi.com>
2022-05-13 13:43:57 +0800

    XYZOP-1775: [feature] qc sms modify

--- app/Services/StudentServices/CollectionService.php
+++ app/Services/StudentServices/CollectionService.php
@@ -4,6 +4,7 @@ namespace App\Services\StudentServices;
 
 use App\Libs\Constants;
 use App\Libs\DictConstants;
+use App\Libs\Dss;
 use App\Libs\NewSMS;
 use App\Libs\Util;
 use App\Services\CommonServiceForApp;
@@ -32,12 +33,13 @@ class CollectionService
     private static function qcDivideIntoClassesMessage($wxNumber, $mobile)
     {
         $content = DictConstants::get(DictConstants::MESSAGES_TEMPLATE, "qc_divide_classes");
-        if (empty($wxNumber) || empty($content)) {
+        if (empty($content)) {
             Util::errorCapture("qing chen divide into classes message params error",
                 ['wx_number' => $wxNumber, 'content' => $content]);
         }
         $smsObj = new NewSMS(DictConstants::get(DictConstants::SERVICE, 'sms_host'));
-        $res = $smsObj->sendCommonSms(Util::pregReplaceTargetStr($content, ['content1' => $wxNumber]), $mobile,
+        $shortUrl = (new Dss())->getShortUrl('http://www.xiaoyezi.com/html/study_piano_download.html')['short_url'];
+        $res = $smsObj->sendCommonSms(Util::pregReplaceTargetStr($content, ['content1' => $shortUrl]), $mobile,
             CommonServiceForApp::SIGN_STUDENT_QC_APP);
         if (empty($res)) {
             Util::errorCapture("qing chen divide into classes message send fail",
