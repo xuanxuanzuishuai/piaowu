515fc6ba459d3cbbd5e15e2f1e56cf0b21be5d73
yuxuan <yuxuan@xiaoyezi.com>
2022-11-26 21:27:09 +0800

    test

--- www/index.php
+++ www/index.php
@@ -25,6 +25,6 @@ require APP_ROOT.'/dependencies.php';
 
 // Register routes
 \App\Routers\RouterFactory::loadRouter($_SERVER['REQUEST_URI'], $app);
-//header('Access-Control-Allow-Headers: X-Requested-With,X_Requested_With');
+header('Access-Control-Allow-Headers: X-Requested-With,X_Requested_With');
 // Run app
 $app->run();
